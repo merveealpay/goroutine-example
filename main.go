de0edcd2d4bdfa80815877fe25289c534adf92f4
merve <merveealpay@gmail.com>
2020-01-10 11:17:47 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+//independent and concurrency: go-routine
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	names := []string{"Merve", "Kumsal", "Cakil", "Deniz"}
+	//add go before function for concurrency
+	go findX(names)
+	go findY(names)
+
+}
+
+func findX(array []string) {
+	for i := 0; i < len(array); i++ {
+		if array[i] == "Kumsal" {
+			fmt.Println("findX found the " + array[i])
+		}
+		time.Sleep(time.Second)
+	}
+}
+func findY(array []string) {
+	for i := 0; i < len(array); i++ {
+		if array[i] == "Kumsal" {
+			fmt.Println("findY found the " + array[i])
+		}
+		time.Sleep(time.Second)
+	}
+}
